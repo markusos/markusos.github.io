03b94d50e2886482c77217a6a445a5f55294d40c
Markus Östberg <markus@ostberg.dev>
2026-04-07 21:03:55 -0400

    Add copy code button

---
+++ assets/js/copy-code.js
@@ -0,0 +1,57 @@
+(function () {
+  var COPY_ICON = '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384c-35.3 0-64-28.7-64-64V64c0-35.3 28.7-64 64-64H361.5c17 0 33.3 6.7 45.3 18.7l23.1 23.1c12 12 18.7 28.3 18.7 45.3V320c0 35.3-28.7 64-64 64H192zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"/></svg>';
+  var CHECK_ICON = '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
+
+  function addCopyButtons() {
+    var blocks = document.querySelectorAll('pre');
+    blocks.forEach(function (pre) {
+      var btn = document.createElement('button');
+      btn.className = 'copy-code-btn';
+      btn.setAttribute('aria-label', 'Copy code');
+      btn.innerHTML = COPY_ICON;
+
+      btn.addEventListener('click', function () {
+        var code = pre.querySelector('code');
+        var text = code ? code.innerText : pre.innerText;
+
+        function onCopied() {
+          btn.innerHTML = CHECK_ICON;
+          btn.classList.add('copied');
+          setTimeout(function () {
+            btn.innerHTML = COPY_ICON;
+            btn.classList.remove('copied');
+          }, 1500);
+        }
+
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+          navigator.clipboard.writeText(text).then(onCopied);
+        } else {
+          var ta = document.createElement('textarea');
+          ta.value = text;
+          ta.style.position = 'fixed';
+          ta.style.opacity = '0';
+          document.body.appendChild(ta);
+          ta.focus();
+          ta.select();
+          try { document.execCommand('copy'); onCopied(); } catch (e) {}
+          document.body.removeChild(ta);
+        }
+      });
+
+      // Wrap pre in a positioned container if not already inside .highlight
+      if (!pre.closest('.highlight')) {
+        var wrapper = document.createElement('div');
+        wrapper.className = 'code-block-wrapper';
+        pre.parentNode.insertBefore(wrapper, pre);
+        wrapper.appendChild(pre);
+        wrapper.appendChild(btn);
+      } else {
+        var highlight = pre.closest('.highlight');
+        highlight.style.position = 'relative';
+        highlight.appendChild(btn);
+      }
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', addCopyButtons);
+})();
